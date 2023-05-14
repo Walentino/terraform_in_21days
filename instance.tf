e5d5159fc9620f4beb66cf6eccd639285294176b
walentino <adewaleodeja@gmail.com>
2023-05-14 00:42:36 -0400

    Using remote state

--- instance.tf
+++ instance.tf
@@ -1,37 +1,53 @@
-data "aws_ami" "ubuntu" {
+locals {
+  mypublicip = sensitive(file("~/.sshmypublic.txt"))
+}
+
+data "aws_ami" "amazonlinux" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["amnz2-ami-kernel-*"]
+    values = ["amzn2-ami-kernel-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
 }
 
 
-
 resource "aws_instance" "public" {
+  
   ami                         = data.aws_ami.amazonlinux.id
   associate_public_ip_address = true
-  instance_type               = "t3.micro"
+  instance_type               = "t2.micro"
   key_name                    = "terra_main"
   vpc_security_group_ids      = [aws_security_group.public.id]
   subnet_id                   = [aws_subnet.public[0].id]
   user_data                   = file("user-data.sh")
-
-
   tags = {
     Name = "${var.env_code}-public"
   }
 }
 
+resource "aws_instance" "private" {
+ 
+  ami                         = data.aws_ami.amazonlinux.id
+  instance_type               = "t2.micro"
+  key_name                    = "terra_main"
+  vpc_security_group_ids      = [aws_security_group.private.id]
+  subnet_id                   = aws_subnet.private[0].id
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
+
 resource "aws_security_group" "public" {
-  name        = "${var.env_code}-public"
-  description = "Allow inbound traffic"
+  name        = "${var.env_code}-publicSG"
+  description = "Allow SSH traffic"
   vpc         = aws_vpc.main.id
 
   ingress {
@@ -39,10 +55,8 @@ resource "aws_security_group" "public" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["184.145.68.143/32"]
+    cidr_blocks = ["local.mypublicip"]
   }
-
-
   ingress {
     description = "HTTP from public"
     from_port   = 80
@@ -50,7 +64,6 @@ resource "aws_security_group" "public" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -58,28 +71,15 @@ resource "aws_security_group" "public" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  
   tags = {
-    name = "${var.env_code}-public"
-  }
-}
-
-
-
-resource "aws_instance" "private" {
-  ami                    = data.aws.amazonlinux.id
-  instance_type          = "t3.micro"
-  key_name               = "terra_main"
-  vpc_security_group_ids = [aws_security_group.private.id]
-  subnet_id              = aws_subnet.private[0].id
-
-  tags = {
-    Name = "${var.env_code}-public"
+    name = "${var.env_code}-publicSG"
   }
 }
 
 resource "aws_security_group" "private" {
-  name        = "${var.env_code}-private"
-  description = "Allow VPC traffic"
+  name        = "${var.env_code}-privateSG"
+  description = "Allow SSH from VPC"
   vpc_id      = aws_vpc.main.id
 
   ingress {
@@ -95,10 +95,11 @@ resource "aws_security_group" "private" {
     to_port     = 0
     protocol    = "tcp"
     cidr_blocks = [var.vpc_cidr]
+  
   }
 
   tags = {
-    Name = "${var.env_code}-private"
+    Name = "${var.env_code}-privateSG"
   }
 }
 
--- providers.tf
+++ providers.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicopr/aws"
-      version = "~> 4.0"
-    }
-  }
-  backend "s3" {
-    bucket         = "terraform-remote-state-wale"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-remote-state"
-  }
-}
-
-#Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
